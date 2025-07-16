a8f2a4ae372142891485c02fb1be63128191314a
tcchase <tylercchase@gmail.com>
2025-07-16 13:32:13 -0800

    feat: formatting and cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -1,81 +1,68 @@
-import {createRoot} from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-import '../src/assets/style.css';
-import { FC, useEffect } from 'react';
-import { Shape, StickyNote, StickyNoteColor } from '@mirohq/websdk-types';
-import { StickyNoteStyle } from '@mirohq/websdk-types/stable/index';
+import "../src/assets/style.css";
+import { FC, useEffect } from "react";
+import {
+  Shape,
+  ShapeStyle,
+} from "@mirohq/websdk-types";
 
-let stickyNotes : {[x: number]: {[y: number]: Shape}}= {
-}
-
-async function addSticky() {
-//   const stickyNote = await miro.board.createStickyNote({
-//     content: 'Hello, World2!',
-//   });
-  const shape = await miro.board.createShape({shape: 'rectangle'})
-  stickyNotes[0] = stickyNotes[0] || {}
-  stickyNotes[0][0] = shape;
-//   setInterval(updateStickyNotes, 1000)
-
-  const videoElement = document.getElementById('bad-apple-video')
-  const canvasElement = document.getElementById('bad-apple-canvas')
-
-//   await miro.board.viewport.zoomTo(stickyNote);
-}
-let toggle = false
-
+let stickyNotes: { [x: number]: { [y: number]: Shape } } = {};
 
 function setupBoard() {
- // we want a 40x40 space of shapes.
- // make it the first time, afterwards grab
- const video = document.getElementById('bad-apple-video') as HTMLVideoElement;
-video.addEventListener('loadeddata', () => {
-	video.play();
-animate()
-})
+  // we want a 40x40 space of shapes.
+  // make it the first time, afterwards grab
+  let shapes = miro.board.get({
+    type: "shape",
+  });
+  console.log(shapes)
+
+  const video = document.getElementById("bad-apple-video") as HTMLVideoElement;
+  video.addEventListener("loadeddata", () => {
+    video.play();
+    animate();
+  });
 }
 
 function animate() {
- const video = document.getElementById('bad-apple-video') as HTMLVideoElement;
- const canvas = document.getElementById('bad-apple-canvas') as HTMLCanvasElement;
- const width = 40;
- const height = 40;
+  const video = document.getElementById("bad-apple-video") as HTMLVideoElement;
+  const canvas = document.getElementById(
+    "bad-apple-canvas"
+  ) as HTMLCanvasElement;
+  const width = 40;
+  const height = 40;
 
- const context = canvas.getContext("2d") as CanvasRenderingContext2D;
- context.drawImage(video, 0,0);
+  const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+  context.drawImage(video, 0, 0);
 
- const imageData = context.getImageData(0,0,width, height)
- const data = imageData.data;
- const step = 1;
- for(let x=0; x < width; x += step) {
-	for(let y = 0; y < height; y += step) {
-		const i = (y * width + x)
-		if(data[i] === 0) {
-			// should show white
-		} else {
-			// should show black (gray?)
-		}
-	}
- }
- requestAnimationFrame(animate)
+  const imageData = context.getImageData(0, 0, width, height);
+  const data = imageData.data;
+  const step = 1;
+  for (let x = 0; x < width; x += step) {
+    for (let y = 0; y < height; y += step) {
+      const i = y * width + x;
+      if (data[i] === 0) {
+        // should show white
+      } else {
+        // should show black (gray?)
+      }
+    }
+  }
+  requestAnimationFrame(animate);
 }
 
-async function updateStickyNotes() {
-	let style: StickyNoteStyle =  {
-	fillColor: toggle ? 'black' : 'gray' as StickyNoteColor, // Default value: light yellow
-	textAlign: 'center', // Default alignment: center
-	textAlignVertical: 'middle', // Default alignment: middle
-	}
-	toggle = !toggle;
-	stickyNotes[0][0].style = style;
-	console.log('test');
-	stickyNotes[0][0].sync();
+function setShapeFill(shape: Shape, isDrawn: bool) {
+  let style = {
+    fillColor: isDrawn ? "#0000ff" : "#ffffff", // Default shape fill color: transparent (no fill)
+    fillOpacity: 1.0, // Default fill color opacity: no opacity
+  } as ShapeStyle;
+  shape.style = style;
 }
+
 
 const App: FC = () => {
   useEffect(() => {
-    // addSticky();
-	setupBoard();
+    setupBoard();
   }, []);
 
   return (
@@ -91,13 +78,17 @@ const App: FC = () => {
           Platform documentation.
         </p>
       </div>
-      <video src='/src/assets/bad_apple.mp4' id='bad-apple-video' className='test' controls>
-	  </video>
-	  <canvas id='bad-apple-canvas'></canvas>
+      <video
+        src="/src/assets/bad_apple.mp4"
+        id="bad-apple-video"
+        className="test"
+        controls
+      ></video>
+      <canvas id="bad-apple-canvas"></canvas>
     </div>
   );
 };
 
-const container = document.getElementById('root') as HTMLElement;
+const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
